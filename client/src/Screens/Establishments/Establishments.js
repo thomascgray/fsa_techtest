c90c15f2b3f43eeb82c2007c5444cec620365b2d
Unknown <tom.c.gray@gmail.com>
2017-09-01 13:39:04 +0100

    starting to wire up an establishments search set

--- client/src/Screens/Establishments/Establishments.js
+++ client/src/Screens/Establishments/Establishments.js
@@ -3,11 +3,44 @@ import React, { Component } from 'react';
 class Establishments extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            name : null,
+            address : null,
+        }
+    }
+
+    updateSearchCriteria() {
+        
     }
 
     render() {
         return (
             <main>
+                <section>
+                    <label>
+                        Establishment Name
+                        <input
+                        onChange={(e) => {
+                            this.setState({name : e.target.value}, () => {
+                                this.updateSearchCriteria();
+                            });
+                        }}
+                        value={this.state.name}
+                        />
+                    </label>
+                    <label>
+                        Establishment Address
+                        <input
+                        onChange={(e) => {
+                            this.setState({address : e.target.value}, () => {
+                                this.updateSearchCriteria();
+                            });
+                        }}
+                        value={this.state.address}
+                        />
+                    </label>
+                </section>
                 <h1>WIP - this page will let you search for establishments when ompleted</h1>
             </main>
         );
