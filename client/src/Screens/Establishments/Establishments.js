0c7b0a15fce702ebd94a2e4666d31d53eef3d685
Unknown <tom.c.gray@gmail.com>
2017-09-01 13:25:37 +0100

    integrating react router

---
+++ client/src/Screens/Establishments/Establishments.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Establishments extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <main>
+                <h1>WIP - this page will let you search for establishments when ompleted</h1>
+            </main>
+        );
+    }
+}
+
+export default Establishments;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+import App from './Screens/App/App';
+import Establishments from './Screens/Establishments/Establishments';
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route exact path="/establishments" component={Establishments}/>
+        </div>
+    </Router>
+	, document.getElementById('root'));
